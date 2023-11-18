db175bafc045b800047cb9d87078501b49e811dc
HKDominator <bodrogean.andrei@gmail.com>
2023-11-18 19:47:43 +0200

    Commit one

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+
+app.set("view engine", "ejs")
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + '/static/index.html')
+})
+
+// Route to Login Page
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/static/login.html')
+})
+
+app.post('/auth', (req, res) => {
+    // Insert Login Code Here
+    let username = req.body.username
+    let password = req.body.password
+    res.send(`Username: ${username} Password: ${password}`)
+  })
+
+
+  
+  const port = 3000
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
